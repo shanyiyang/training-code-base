09dbe4eea32233c75532e6cc48d17afbeabc7573
Shan Yi - Yang <action5351545@gmail.com>
2017-11-28 21:00:12 +0800

    Add a socket server using select avoid blocking

---
+++ socket/select/udp/server/src/select_udp_simple_server.c
@@ -0,0 +1,105 @@
+/***********************************************/
+/* This is a simple UDP server.                */
+/* Use select blocking function to read data.  */
+/***********************************************/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define MAXBUFFERSIZE 1024
+
+int main()
+{
+	/* Define variable */
+	struct sockaddr_in	sock_addr;
+	struct sockaddr_in 	addr_src;
+	int 				local_sock;
+	socklen_t 			addr_size;
+	int 				read_size = 0;
+	int 				rand_num;
+	int 				count = 0;
+	int 				read_num;
+	int 				maxfds;
+	int 				i;
+	char 				outbuf[MAXBUFFERSIZE];
+	char 				inbuf[MAXBUFFERSIZE];
+	fd_set 				active_fds;
+	fd_set				read_fds;
+	
+	// Initial random number.
+	srand(time(NULL));
+	
+	/* Initial variable */
+	// Initial variables.
+	memset(&sock_addr, 0, sizeof(struct sockaddr_in));		// Clear struct sockaddr_in variable
+	sock_addr.sin_family = AF_INET;						// AF_INET: Address Family
+	sock_addr.sin_addr.s_addr = inet_addr("127.0.0.1");	// IP address
+	sock_addr.sin_port = htons(5678);						// Port number
+	
+	// Create local_socket.
+	if ((local_sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		perror("socket");
+		exit(1);
+	}
+	printf("Create socket successful!\n");
+	
+	// Bind()
+	if (bind(local_sock, (struct sockaddr *) &sock_addr, sizeof(sock_addr)) < 0)
+	{
+		perror("bind");
+		exit(1);
+	}
+	printf("Bind socket successful!\n");
+	
+	// Monitor fds.
+	FD_ZERO(&active_fds);
+	FD_SET(local_sock, &active_fds);
+	maxfds = local_sock;
+	
+	/* Procedure */
+	while (count < 10)
+	{
+		read_fds = active_fds;
+		if (select(maxfds + 1, &read_fds, NULL, NULL, NULL) < 0)
+		{
+			perror("Select");
+		}
+		else
+		{
+			for (i = 0; i < FD_SETSIZE; i++)
+			{
+				if (FD_ISSET(i, &read_fds))
+				{
+					// Read a data from socket.
+					read_size = recvfrom(i, inbuf, sizeof(int), 0, 
+							(struct sockaddr*) &addr_src, &addr_size);
+					memcpy(&read_num, inbuf, read_size);
+					printf("Read number: %d\n", read_num);
+						
+					// Write a data by socket
+					rand_num = rand() % 100;
+					printf("Send number: %d\n", rand_num);
+					memcpy(outbuf, &rand_num, sizeof(int));
+					sendto(i, outbuf, sizeof(int), 0, 
+							(struct sockaddr*) &addr_src, sizeof(addr_src));
+						
+					count ++;
+				}
+			}
+		}
+	}
+	
+	close(local_sock);
+	
+	return 0;
+}
